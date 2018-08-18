8d09eca099300a13246e51e724ba0699cdcc8e52
Adedoyin Yinka <timmytune002@gmail.com>
2018-08-18 11:52:24 +0100

    comit2

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,8 +5,16 @@ include("./init.php");
 // doc::set_wigets(doc::$top_modules_single, TemplateEngine::get2('yink_home',array('type'=>'header')),1);
   
 //doc::set_wigets(doc::$left, TemplateEngine::get2('yink_home',array('type'=>'left')),1);
-  doc::set_wigets(doc::$center, TemplateEngine::get2('yink_home',array('type'=>'right')),1);
-  doc::set_wigets(doc::$top_modules_single, TemplateEngine::get2('yink_home',array('type'=>'header')),1);
 
+
+
+if(wire('pages')->find("template=app") < 4){
+   doc::set_wigets(doc::$center, TemplateEngine::get2('yink_home',array('type'=>'right')),1);
+  doc::set_wigets(doc::$top_modules_single, TemplateEngine::get2('yink_home',array('type'=>'header')),1);
+ 
+}else{
+    echo "<hi> Application submision has closed </hi>";
+}
+  
 
 include("./main.php"); 
